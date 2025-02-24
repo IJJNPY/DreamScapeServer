209b9964eb0016a7c95f93c4a0b466e4446228ed
IJJNPY <88335957+IJJNPY@users.noreply.github.com>
2025-02-24 15:48:56 +0800

    database schema update && banner function update

---
+++ DreamScapeServer/pages/activity/daylist.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="">
+		<custom-head-top>
+			<template #left>
+				每日推荐
+			</template>
+		</custom-head-top>
+		
+		<view class="grid">
+			<view class="item add">
+				+
+			</view>
+			<view class="item" v-for="item in 10">
+				自定义组件
+			</view>
+		</view>
+		
+		<view class="paging">
+			分页器
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
